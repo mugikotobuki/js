203d701c6b89101c5eff70cb348b8656f1928104
mugikotobuki <juse1julia-s@mail.ru>
2019-06-14 12:55:00 +0300

    Выполнено задание 2 из урока 4 (без проверки стенок и ходьбы по диагонали)

---
+++ dz_4/game_walking/player.js
@@ -0,0 +1,14 @@
+'use strict';
+const player = {
+    x: 0,
+    y: 0,
+
+    /**
+     * Двигает игрока по переданному направлению.
+     * @param {{x: int, y: int}} nextPoint Следующая точка пользователя.
+     */
+    move(nextPoint) {
+        this.x = nextPoint.x;
+        this.y = nextPoint.y;
+    },
+};
